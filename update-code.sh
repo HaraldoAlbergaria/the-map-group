8cb0c9fca8d3ff4de9b3d6fef992c7b0c87160ef
HaraldoFilho <haraldo.filho@gmail.com>
2025-01-21 12:16:50 -0300

    Updated code [Added variable use_matrix to countries_info.py]

--- update-code.sh
+++ update-code.sh
@@ -10,6 +10,7 @@ cd ../The-Map-Group
 git checkout -- .
 cp ../the-map-group.github.io/countries_bbox.js .
 cp ../the-map-group.github.io/countries_info.py .
+cp ../the-map-group.github.io/config.py .
 cp ../the-map-group.github.io/custom.js .
 cp ../the-map-group.github.io/generate-map-data.py .
 cp ../the-map-group.github.io/index.html .
